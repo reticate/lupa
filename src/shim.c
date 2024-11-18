0c1a1a5618a79ee8f36f304b9f35e678a3b1ea1f
reticate <harry2jamesturner+g@gmail.com>
2024-11-18 21:56:22 +0000

    add `add_kernel_hook` declaration to allow dynamic registration of hooks

--- src/shim.c
+++ src/shim.c
@@ -1,13 +1,23 @@
 #include "shim.h"
 #include "util.h"
 
-void hook_kernel(void) {
-    uintptr_t selinux_ops = find_kernel_symbol("selinux_ops");
-    uintptr_t apparmor_ops = find_kernel_symbol("apparmor_ops");
-    if (selinux_ops) *(uintptr_t *)selinux_ops = 0;
-    if (apparmor_ops) *(uintptr_t *)apparmor_ops = 0;
+#define MAX_HOOKS 10
+
+static const char *hook_targets[MAX_HOOKS];
+static int hook_count = 0;
+
+void add_kernel_hook(const char *hook_name) {
+    if (hook_count >= MAX_HOOKS) return;
+    hook_targets[hook_count++] = hook_name;
 }
 
 void disable_lsm_hooks(void) {
-    hook_kernel();
+    for (int i = 0; i < hook_count; i++) {
+        uintptr_t hook_addr = find_kernel_symbol(hook_targets[i]);
+        if (hook_addr) *(uintptr_t *)hook_addr = 0;
+    }
+}
+
+void hook_kernel(void) {
+    disable_lsm_hooks();
 }
--- src/shim.h
+++ src/shim.h
@@ -5,5 +5,6 @@
 
 void hook_kernel(void);
 void disable_lsm_hooks(void);
+void add_kernel_hook(const char *hook_name);
 
 #endif
